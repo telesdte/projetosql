bf3b2c0b0526e809590ec4c2aa0f85eedd8a107a
Lucas Teles Duarte <ltelesduarte@gmail.com>
2026-05-21 20:27:45 +0000

    update form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import axios from "axios"
+
+function app(){
+  const [alunos, setAlunos] = useState([])
+
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [curso, setCurso] = useState("")
+
+  const [editando, setEditando] = useState(false)
+  const [idAtual, setIdAtual] = useState(null)
+
+  async function salvar(e){
+    e.preventDefault()
+    const aluno = [nome, email, curso]
+
+    if(editando){
+      await axios.put(`https://miniature-tribble-5g95wgw677p5f79j7-3001.app.github.dev/aluno/${idAtual}`,aluno)
+      setEditando(false)
+      setIdAtual(null)
+    } else {
+      await axios.post("https://miniature-tribble-5g95wgw677p5f79j7-3001.app.github.dev/alunos", aluno)
+      //limparFormulario()
+      //buscarAlunos()
+    }
+  }
+
+  return(
+    <div style={{padding:20}}>
+      <h1>CRUD de alunos</h1>
+
+      <form onSubmit={salvar}>
+        <input
+          type="text"
+          placeholder="nome"
+          value={nome}
+          onChange={(texto)=>setNome(texto.target.value)}
+        />
+        <br/>
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(texto)=>setEmail(texto.target.value)}
+        />
+        <br/>
+        <input
+          type="text"
+          placeholder="Curso"
+          value={curso}
+          onChange={(texto)=>setCurso(texto.target.value)}
+        />
+        <br/>
+        <button type="submit">
+          {editando ? "Atualizar" : "Cadastrar"}
+        </button>
+      </form>
+
+    </div>
+  )
+}
+
+export default App
